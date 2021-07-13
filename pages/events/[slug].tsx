e10f051acf29630cae9a47fd78ef89a8d3e87e13
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-13 12:19:31 +0300

    Add Event pages

---
+++ pages/events/[slug].tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Layout } from "components/common/Layout";
+
+export default function EventPage() {
+	return <Layout>Event Page</Layout>;
+}
---
+++ pages/events/add.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Layout } from "components/common/Layout";
+
+export default function AddPage() {
+	return <Layout title="Add">Add Event</Layout>;
+}
