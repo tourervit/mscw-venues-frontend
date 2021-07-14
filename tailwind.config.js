7ac203b20730f428b87e45299787df2d3cb66459
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-14 20:25:09 +0300

    Add flicker animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
 	darkMode: "class", // or 'media' or 'class'
 	theme: {
 		extend: {
+			animation: {
+				flicker: "flicker 15s ease-in infinite",
+			},
 			colors: {
 				pink: {
 					950: "#40152f",
@@ -12,6 +15,28 @@ module.exports = {
 			},
 			fontFamily: {
 				sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+			},
+			keyframes: {
+				flicker: {
+					"0%": { filter: "grayscale(0.8)" },
+					"8%": { filter: "grayscale(0.1)" },
+					"10%": { filter: "grayscale(0.7)" },
+					"40%": { filter: "grayscale(0.2)" },
+					"42%": { filter: "grayscale(0.7)" },
+					"48%": { filter: "grayscale(0.1)" },
+					"52%": { filter: "grayscale(0.6)" },
+					"56%": { filter: "grayscale(0)" },
+					"59%": { filter: "grayscale(0.9)" },
+					"62%": { filter: "grayscale(0)" },
+					"65%": { filter: "grayscale(0.1)" },
+					"70%": { filter: "grayscale(0.6)" },
+					"77%": { filter: "grayscale(0)" },
+					"83%": { filter: "grayscale(0.7)" },
+					"86%": { filter: "grayscale(0.9)" },
+					"88%": { filter: "grayscale(0)" },
+					"96%": { filter: "grayscale(0.6)" },
+					"100%": { filter: "grayscale(0.7)" },
+				},
 			},
 			screens: {
 				xs: "475px",
