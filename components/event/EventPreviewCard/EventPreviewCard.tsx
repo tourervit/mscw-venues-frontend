84802f0ce70ecbe8744ac9b2edd01506c9ee9a3e
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-17 16:08:19 +0300

    Fix mobile gradients

--- components/event/EventPreviewCard/EventPreviewCard.tsx
+++ components/event/EventPreviewCard/EventPreviewCard.tsx
@@ -11,18 +11,18 @@ function EventPreviewCard({ event }: EventPreviewCardProps) {
 	return (
 		<Link href={`/${event.slug}`}>
 			<a className="group">
-				<div className="px-4 sm:px-8 relative mb-10 w-full h-72 bg-gradient-to-r from-black via-gray-600 to bg-gray-800 ">
+				<div className="px-4 sm:px-8 relative mb-10 w-full h-72 bg-gradient-to-r from-black via-gray-600 to-gray-800 ">
 					<Image
 						src={event.image}
 						layout="fill"
 						objectFit="cover"
 						alt={event.name}
-						className="group-hover:scale-105 origin-top transition-all group-hover:opacity-30 duration-300"
+						className="sm:group-hover:scale-105 origin-top transition-all opacity-50 sm:opacity-100 sm:group-hover:opacity-30 duration-300"
 					/>
-					<div className="absolute top-1/2 font-light text-xl sm:text-2xl  leading-6 text-white opacity-0 group-hover:opacity-100 group-hover:-translate-y-1/2 duration-500">
+					<div className="absolute top-1/2 font-light text-xl sm:text-2xl leading-6 text-white sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-500">
 						{event.name}
 					</div>
-					<div className="absolute bottom-1/3 text-xs font-light text-gray-300 opacity-0 group-hover:opacity-100 group-hover:-translate-y-1/2 duration-700">
+					<div className="absolute bottom-1/3 text-xs font-light text-gray-300 sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-700">
 						{event.date} at {event.time}
 					</div>
 				</div>
