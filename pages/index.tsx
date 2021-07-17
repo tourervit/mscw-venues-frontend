7f913cf0d501bc445f81e1f3dace8b9632a79174
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-17 15:47:09 +0300

    Add Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { Layout } from "components/common/Layout";
+import heroPic from "public/hero.jpg";
 
 export default function HomePage() {
-	return <Layout>Home page</Layout>;
+	return (
+		<Layout>
+			<div className="relative w-full h-80 mb-36">
+				<Image
+					src={heroPic}
+					alt="girl in a night club"
+					className="animate-flicker"
+					layout="fill"
+					objectFit="cover"
+					priority
+				/>
+			</div>
+		</Layout>
+	);
 }
