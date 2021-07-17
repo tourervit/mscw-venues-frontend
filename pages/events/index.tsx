1148079952d5ad9f972a5d2ec64632e257a2dbef
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-17 15:16:23 +0300

    Complete Events Page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,6 +1,37 @@
 import React from "react";
+import { GetStaticPropsResult, NextPageContext } from "next";
 import { Layout } from "components/common/Layout";
+import { Event } from "components/event/types";
+import { API_URL } from "config";
+import { EventPreviewCard } from "components/event/EventPreviewCard";
 
-export default function EventsPage() {
-	return <Layout title="Events">Events page</Layout>;
+interface EventsPageProps {
+	events: Event[];
+}
+
+export default function EventsPage({ events }: EventsPageProps) {
+	return (
+		<Layout title="Events">
+			<h1 className="mb-6 text-2xl font-light text-center">Upcoming</h1>
+			{events.length === 0 && <p>Sorry, no events</p>}
+			<div className="max-w-6xl mx-auto">
+				{events?.map(event => (
+					<EventPreviewCard key={event.id} event={event} />
+				))}
+			</div>
+		</Layout>
+	);
+}
+
+export async function getStaticProps(
+	ctx: NextPageContext,
+): Promise<GetStaticPropsResult<EventsPageProps>> {
+	const response = await fetch(`${API_URL}/api/events`);
+	const data: { events: Event[] } = await response.json();
+	return {
+		props: {
+			events: data.events,
+		},
+		revalidate: 5,
+	};
 }
