eebabedd546cf677fe038e4d5e44f9d77538076e
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-18 11:28:44 +0300

    Add background tint

--- components/event/EventPreviewCard/EventPreviewCard.tsx
+++ components/event/EventPreviewCard/EventPreviewCard.tsx
@@ -12,19 +12,22 @@ interface EventPreviewCardProps {
 function EventPreviewCard({ event, className }: EventPreviewCardProps) {
 	return (
 		<Link href={`/${event.slug}`}>
-			<a className={cn("block relative group px-4 sm:px-8", className)}>
+			<a className={cn("block relative group", className)}>
 				<Image
 					src={event.image}
 					layout="fill"
 					objectFit="cover"
 					alt={event.name}
-					className="sm:group-hover:scale-105 origin-top transition-all opacity-50 sm:opacity-100 sm:group-hover:opacity-30 duration-300"
+					className="sm:group-hover:scale-105 origin-top transition-all duration-300"
 				/>
-				<div className="absolute top-1/2 font-light text-xl sm:text-2xl leading-6 text-white sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-500">
-					{event.name}
-				</div>
-				<div className="absolute bottom-1/3 text-xs font-light text-gray-300 sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-700">
-					{event.date} at {event.time}
+				<div className="bg-black/60 md:bg-black/0 md:group-hover:bg-black/40 absolute md: w-full h-full" />
+				<div className="absolute top-1/2 px-4 sm:px-6">
+					<h3 className="mb-2 xs:mb-0 font-light text-2xl leading-6 text-white sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-2/3 duration-500">
+						{event.name}
+					</h3>
+					<div className="text-xs font-light text-gray-300 sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-500">
+						{event.date} at {event.time}
+					</div>
 				</div>
 			</a>
 			{/* <div className="relative max-w-5xl w-full h-56 rounded-sm overflow-hidden bg-gradient-to-r from-gray-400 via-pink-400 to-gray-800">
