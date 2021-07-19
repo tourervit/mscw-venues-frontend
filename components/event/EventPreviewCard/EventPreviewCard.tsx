1e8839613e1b57ca70431a7da69fdae71c5f7ac3
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-19 14:00:13 +0300

    Fix breakpoints for large devices

--- components/event/EventPreviewCard/EventPreviewCard.tsx
+++ components/event/EventPreviewCard/EventPreviewCard.tsx
@@ -20,16 +20,16 @@ function EventPreviewCard({ event, className }: EventPreviewCardProps) {
 					layout="fill"
 					objectFit="cover"
 					placeholder="blur"
-					className="sm:group-hover:scale-105 origin-top transition-all duration-300"
+					className="lg:group-hover:scale-105 origin-top transition-all duration-300"
 					blurDataURL={event.image.formats.thumbnail.url}
 				/>
-				<div className="bg-gradient-to-b from-transparent via-transparent sm:to-transparent to-black md:bg-black/0 md:group-hover:bg-black/40 absolute md: w-full h-full" />
-				<div className="absolute bottom-0 sm:top-1/2 px-4 pb-6 sm:p-8">
-					<h3 className="mb-1 sm:mb-0 font-light text-xl sm:text-2xl leading-5 text-white sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/3 duration-500">
+				<div className="bg-gradient-to-b from-transparent via-transparent lg:to-transparent to-black lg:bg-black/0 lg:group-hover:bg-black/40 absolute w-full h-full" />
+				<div className="absolute bottom-0 lg:top-1/2 px-4 pb-6 lg:p-8">
+					<h3 className="mb-1 lg:mb-0 font-light text-xl lg:text-2xl leading-5 text-white lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:-translate-y-1/3 duration-500">
 						{event.name}
 					</h3>
-					<div className="text-xs font-light text-gray-300 sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1/2 duration-300">
-						{dateformat(event.date, "dddd, mmmm yyyy @ h:MM TT")}
+					<div className="text-xs font-light text-gray-300 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:-translate-y-1/2 duration-300">
+						{dateformat(event.date, "dddd, dd mmmm yyyy @ h:MM TT")}
 					</div>
 				</div>
 			</a>
