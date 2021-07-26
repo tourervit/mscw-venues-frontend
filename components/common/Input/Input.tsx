8e72a6cbfac8764a390906bad6570e4419984a25
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-26 14:40:48 +0300

    Create Input component

---
+++ components/common/Input/Input.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { UseFormRegister, RegisterOptions } from "react-hook-form";
+
+interface InputProps {
+	register: Function;
+	fieldName: string;
+	type: "text" | "password";
+	validation: RegisterOptions;
+}
+
+function Input({ register, fieldName, type, validation }: InputProps) {
+	return (
+		<input
+			id={fieldName}
+			type={type}
+			placeholder={fieldName}
+			className="peer rounded-none w-full bg-white border-b-[1px] placeholder-transparent focus:outline-none border-gray-300 dark:border-gray-400 focus:border-black dark:bg-black dark:focus:border-white"
+			{...register(fieldName, validation)}
+		/>
+	);
+}
+
+export default Input;
