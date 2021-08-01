1fb94f34e878c469b2b78c63cbc59a6a4c586a49
Vitaliy Ponomarev <tourervit@gmail.com>
2021-08-01 18:43:31 +0300

    Offline handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "react-hot-toast";
@@ -5,18 +6,29 @@ import { DefaultSeo } from "next-seo";
 import seo from "seo.config";
 import "../styles/globals.css";
 import { AuthProvider } from "context/auth-context";
+import { Offline, Online } from "react-detect-offline";
 
 const defaultTheme = "light";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider attribute="class" defaultTheme={defaultTheme}>
-			<DefaultSeo {...seo} />
-			<AuthProvider>
-				<Component {...pageProps} />
-			</AuthProvider>
-			<Toaster />
-		</ThemeProvider>
+		<>
+			<Online>
+				<ThemeProvider attribute="class" defaultTheme={defaultTheme}>
+					<DefaultSeo {...seo} />
+					<AuthProvider>
+						<Component {...pageProps} />
+					</AuthProvider>
+					<Toaster />
+				</ThemeProvider>
+			</Online>
+			<Offline>
+				<div className="pt-28 h-screen text-center uppercase ">
+					<span className="italic">No internet on this device</span>
+					<span className="text-2xl">&nbsp; :&#40;</span>
+				</div>
+			</Offline>
+		</>
 	);
 }
 
