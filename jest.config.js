3b20a6d1d16d68001c6358dd942ff1259ee2a962
Vitaliy Ponomarev <tourervit@gmail.com>
2021-08-02 20:58:57 +0300

    FIx test config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
 	setupFilesAfterEnv: ["./setupTests.js"],
 	modulePaths: ["<rootDir>"],
 	testEnvironment: "jsdom",
+	moduleNameMapper: {
+		".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "identity-obj-proxy",
+	},
 };
