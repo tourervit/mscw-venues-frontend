9625a071f8fd0bc40560ae8eba292d534728d9aa
Vitaly <tourervit@gmail.com>
2021-08-06 11:03:46 +0300

    Add dashboard dummy

---
+++ pages/dashboard.tsx
@@ -0,0 +1,6 @@
+import { Layout } from "components/common/Layout";
+import React from "react";
+
+export default function DashboardPage() {
+	return <Layout description="Dashboard">Dashboard</Layout>;
+}
