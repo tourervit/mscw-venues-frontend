c81e4e17959c880bc55c6de7ea8cf7834a2535f2
Vitaly <tourervit@gmail.com>
2021-08-06 13:34:57 +0300

    Add classname customization

--- components/form/Label/Label.tsx
+++ components/form/Label/Label.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import cn from "classnames";
 
 interface LabelProps {
 	children: React.ReactNode;
 	htmlFor: string;
+	className?: string;
 }
 
-function Label({ children, htmlFor }: LabelProps) {
+function Label({ children, htmlFor, className }: LabelProps) {
 	return (
 		<label
-			className="absolute left-0 -top-5 cursor-pointer font-light text-xs text-gray-600 dark:text-gray-300 peer-placeholder-shown:top-0 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 dark:peer-placeholder-shown:text-gray-500 transition-all"
+			className={cn(
+				"absolute left-0 -top-5 cursor-pointer font-light text-xs text-gray-600 dark:text-gray-300 peer-placeholder-shown:top-0 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 dark:peer-placeholder-shown:text-gray-500 transition-all",
+				className,
+			)}
 			htmlFor={htmlFor}
 		>
 			{children}
