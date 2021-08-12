00ecdb8766becc3af1791fc853f2c586c61116bd
Vitaly <tourervit@gmail.com>
2021-08-12 20:35:51 +0300

    Revert "Test"
    This reverts commit 41917127585cbb2ad31c7caa5ca3d56ea0e9dbc8.

---
+++ components/common/NavLink/NavLink.test.tsx
@@ -0,0 +1,34 @@
+import { screen, render, mockNextUseRouter } from 'test/test-utils';
+import NavLink from './NavLink';
+
+describe('NavLink', () => {
+	test('has correct href in <a> tag', () => {
+		mockNextUseRouter({
+			route: '/',
+		});
+		render(<NavLink href="/about">About</NavLink>);
+		const lnk = screen.getByRole('link', { name: /about/i });
+		expect(lnk).toBeInTheDocument();
+		expect(lnk).toHaveAttribute('href', '/about');
+	});
+
+	test('changes color on hover', () => {
+		mockNextUseRouter({
+			route: '/',
+		});
+		render(<NavLink href="/about">About</NavLink>);
+		const lnk = screen.getByRole('link', { name: /about/i });
+		expect(lnk).toHaveClass(
+			'text-gray-500 dark:text-gray-300 hover:text-black dark:hover:text-white',
+		);
+	});
+
+	test('has different color on active route', () => {
+		mockNextUseRouter({
+			route: '/about',
+		});
+		render(<NavLink href="/about">About</NavLink>);
+		const about = screen.getByRole('link', { name: /about/i });
+		expect(about).toHaveClass('text-black dark:text-white');
+	});
+});
