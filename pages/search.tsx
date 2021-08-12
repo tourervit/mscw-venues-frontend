96a98d5d88e6943c1481f7b90c9fd74b6c01ff6a
Vitaly <tourervit@gmail.com>
2021-08-12 15:06:14 +0300

    Adjust Search component

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
 import {
 	GetServerSidePropsResult,
 	GetStaticPropsResult,
 	GetStaticPropsContext,
 	GetServerSidePropsContext,
-} from "next";
-import qs from "qs";
-import { useRouter } from "next/dist/client/router";
-import { Layout } from "components/common/Layout";
-import { EventData } from "components/event/event-types";
-import { API_URL } from "config";
-import { EventPreviewCard } from "components/event/EventPreviewCard";
+} from 'next';
+import qs from 'qs';
+import { useRouter } from 'next/dist/client/router';
+import { Layout } from 'components/common/Layout';
+import { EventData } from 'components/event/event-types';
+import { API_URL } from 'config';
+import { EventPreviewCard } from 'components/event/EventPreviewCard';
+import { PageTitle } from 'components/common/PageTitle';
 
 interface EventsPageProps {
 	events: EventData[];
@@ -23,10 +24,12 @@ export default function EventsPage({ events }: EventsPageProps) {
 	return (
 		<Layout title="Events">
 			<div className="max-w-6xl mx-auto">
-				<h1 className="mb-6 px-4 text-3xl font-light">
-					Search results for <span className="font-normal">"{q}"</span>
-				</h1>
-				{events.length === 0 && <p className="px-4">Sorry, no events</p>}
+				<div className="px-4">
+					<PageTitle>
+						Search results for <span className="font-normal">"{q}"</span>
+					</PageTitle>
+					{events.length === 0 && <p>Sorry, no events</p>}
+				</div>
 				{events?.map(event => (
 					<EventPreviewCard key={event.id} event={event} className="w-full h-72 mb-10" />
 				))}
